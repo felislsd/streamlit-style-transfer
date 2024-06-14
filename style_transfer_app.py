8917b3ad556a317e344e1eb3508697c13e0d585b
Joanna Dziekanska <dziekanska.joanna@gmail.com>
2024-06-14 08:41:07 +0200

    Add image processing functions

--- style_transfer_app.py
+++ style_transfer_app.py
@@ -24,3 +24,37 @@ def load_vgg19_model():
     return model
 
 vgg_model = load_vgg19_model()
+
+
+# Image processing functions
+def load_and_process_image(image_path):
+    img = load_img(image_path)
+    img = img_to_array(img)
+    img = preprocess_input(img)
+    img = np.expand_dims(img, axis=0) #model expects 4dim tensor
+    return img
+
+def load_and_process_image_url(image_url):
+    response = requests.get(image_url)
+    img = Image.open(BytesIO(response.content))
+    img = img_to_array(img)
+    img = preprocess_input(img)
+    img = np.expand_dims(img, axis=0)
+    return img
+
+def deprocess(x):
+    x[:, :, 0] += 103.939 # does the opposite of preprocess_input
+    x[:, :, 1] += 116.779
+    x[:, :, 2] += 123.68
+    x = x[:, :, ::-1]  # invert the order of the channels
+    x = np.clip(x, 0, 255).astype('uint8')
+    return x
+
+def display_image(image):
+    img = np.squeeze(image, axis=0)
+    img = deprocess(img)
+    plt.grid(False)
+    plt.xticks([])
+    plt.yticks([])
+    plt.imshow(img)
+    st.pyplot(plt)
